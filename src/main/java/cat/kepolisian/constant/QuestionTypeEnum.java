c42184a27185f0a0ae80539ba61318a52c08c294
mustajab9 <mustajabsa@gmail.com>
2024-01-14 18:17:44 +0700

    Change Getter Enum using Lombok

--- src/main/java/cat/kepolisian/constant/QuestionTypeEnum.java
+++ src/main/java/cat/kepolisian/constant/QuestionTypeEnum.java
@@ -1,21 +1,17 @@
 package cat.kepolisian.constant;
 
+import lombok.Getter;
+
+@Getter
 public enum QuestionTypeEnum {
 	KEPRIBADIAN("kepribadian", "Q01"), AKADEMIK("Akademik", "Q02"), KETAHANAN("Ketahanan", "Q03");
 
-	private String detail;
-	private String code;
+	private final String detail;
+	private final String code;
 
 	private QuestionTypeEnum(String detail, String code){
 		this.detail = detail;
 		this.code = code;
 	}
-	
-	public String getDetail() {
-		return this.detail;
-	}
-	
-	public String getCode() {
-		return this.code;
-	}
+
 }
--- src/main/java/cat/kepolisian/constant/RoleEnum.java
+++ src/main/java/cat/kepolisian/constant/RoleEnum.java
@@ -1,21 +1,17 @@
 package cat.kepolisian.constant;
 
+import lombok.Getter;
+
+@Getter
 public enum RoleEnum {
 	ADMIN("admin", "U01"), USER("user", "U02");
 	
-	private String detail;
-	private String code;
+	private final String detail;
+	private final String code;
 	
 	private RoleEnum(String detail, String code){
 		this.detail = detail;
 		this.code = code;
 	}
-	
-	public String getDetail() {
-		return this.detail;
-	}
-	
-	public String getCode() {
-		return this.code;
-	}
+
 }
